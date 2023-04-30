625d7dfce7a7599210d2249b80ed72ad6e8ed970
Daniela Gómez Estrella <60994358+danigoes@users.noreply.github.com>
2023-04-29 19:32:51 -0500

    debit and credit methods created and tested

--- junit_app/src/main/java/org/udemy/junit_app/models/Account.java
+++ junit_app/src/main/java/org/udemy/junit_app/models/Account.java
@@ -14,4 +14,25 @@ import java.math.BigDecimal;
 public class Account {
     private String person;
     private BigDecimal balance;
+
+    public void debit(BigDecimal value) {
+        BigDecimal newBalance = this.balance.subtract(value);
+        this.balance = newBalance;
+    }
+
+    public void credit(BigDecimal value) {
+        this.balance = this.balance.add(value);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof Account)) {
+            return false;
+        }
+        Account c = (Account) obj;
+        if (this.person == null || this.balance == null) {
+            return false;
+        }
+        return this.person.equals(c.getPerson()) && this.balance.equals(c.getBalance());
+    }
 }
